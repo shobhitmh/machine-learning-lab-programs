89779558901627bd2102d29a5eeb0e0bae911b31
Shobhit M <118930409+shobhitmh@users.noreply.github.com>
2024-03-30 18:35:41 +0530

    Best First Search

---
+++ best-first-search.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def best_first_search(graph,start,goal,heuristic, path=[]):
+    open_list = [(0,start)]
+    closed_list = set()
+    closed_list.add(start)
+
+    while open_list:
+        open_list.sort(key = lambda x: heuristic[x[1]], reverse=True)
+        cost, node = open_list.pop()
+        path.append(node)
+
+        if node==goal:
+            return cost, path
+
+        closed_list.add(node)
+        for neighbour, neighbour_cost in graph[node]:
+            if neighbour not in closed_list:
+                closed_list.add(node)
+                open_list.append((cost+neighbour_cost, neighbour))
+
+    return None
+
+
+graph = {
+    'A': [('B', 11), ('C', 14), ('D',7)],
+    'B': [('A', 11), ('E', 15)],
+    'C': [('A', 14), ('E', 8), ('D',18), ('F',10)],
+    'D': [('A', 7), ('F', 25), ('C',18)],
+    'E': [('B', 15), ('C', 8), ('H',9)],
+    'F': [('G', 20), ('C', 10), ('D',25)],
+    'G': [],
+    'H': [('E',9), ('G',10)]
+}
+
+start = 'A'
+goal = 'G'
+
+heuristic = {
+    'A': 40,
+    'B': 32,
+    'C': 25,
+    'D': 35,
+    'E': 19,
+    'F': 17,
+    'G': 0,
+    'H': 10
+}
+
+result = best_first_search(graph, start, goal, heuristic)
+
+if result:
+    print(f"Minimum cost path from {start} to {goal} is {result[1]}")
+    print(f"Cost: {result[0]}")
+else:
+    print(f"No path from {start} to {goal}")
+
+
+# In[ ]:
+
+
+
+
